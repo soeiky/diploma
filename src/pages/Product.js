e2390b7d13d8a5bb3b45d6fbd2f9048dde8c632e
soeiky <bapaevatunuk@gmail.com>
2022-06-30 03:08:50 +0600

    add product css

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,26 +2,25 @@ import { useParams } from "react-router-dom";
 import CartButton from "../Components/CartButton/CartButton";
 import Header from "../Components/Header/Header"
 import { getProduct} from "../data/Products";
+import  "./Product.css";
 
 function Product() {
   const params = useParams();
   const product = getProduct(params.productId);
-
 
   if (!product) {
     return null;
   }
 
   return (
-    <>
-      <Header
-        title={product.title}
-        image={product.image}>
+    <div className="Product">
+      <Header image={product.image} title={product.title}>
+        <div className="price">{product.price}</div>
+        
+
         {product.description}
       </Header>
-      <CartButton productId={params.productId} />
-     
-    </>
+    </div>
   );
 }
 
