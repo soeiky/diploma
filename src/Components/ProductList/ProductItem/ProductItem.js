c50ecb40ae490a7ca9e65bb788eb03e08b078fa0
soeiky <bapaevatunuk@gmail.com>
2022-06-29 23:52:53 +0600

    add styles, components

--- src/Components/ProductList/ProductItem/ProductItem.js
+++ src/Components/ProductList/ProductItem/ProductItem.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
-import classes from "./ProductItem.module.css"
+import classes from "./ProductItem.module.css";
 
 function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
-      <img src={product.image} alt={product.title} />
-      <Link to={"/products/" + product.productId}>{product.title}</Link>
-      <div className={classes.price}>${product.price}</div>
+      <a href={"/products/" + product.productId}><img src={product.image} alt="img" /></a>
+      <div className={classes.container}>
+        <Link to={"/products/" + product.title}>{product.productId} </Link>
+        <div className={classes.price}>{product.price}Som</div>
+       
+
+      </div>
     </div>
   );
 }
