74ade1e87963e2e44380c949513d96ebc5b31529
soeiky <bapaevatunuk@gmail.com>
2022-06-29 23:38:18 +0600

    add drawer comp

--- src/Components/CategoryList/CategoryItem/CategoryItem.js
+++ src/Components/CategoryList/CategoryItem/CategoryItem.js
@@ -4,9 +4,9 @@ import classes from "./CategoryItem.module.css";
 function CategoryItem({ category }) {
   return (
     <div className={classes.CategoryItem}>
-      <img src={category.image} alt={category.title} />
       <div className={classes.container}>
-        <Link to={"/categories/" + category.categoryId}>{category.title} </Link>
+        <Link to={"/categories/" + category.categoryId}>  <img src={category.image} alt={category.title}/> </Link>
+        <div className="title"><Link to={"/categories/" + category.categoryId}>{category.title} </Link></div>
       
       </div>
     </div>
